db3d8a880510cec3a50fd9f7242c23f1e378b63d
Elias Bukreev <elias.activity@gmail.com>
2025-10-08 14:04:38 +1000

    keep working on PeriodicControl

--- src/data/Periodic control.ts
+++ src/data/Periodic control.ts
@@ -20,44 +20,52 @@ export const schema = [
     },
   },
   {
-    $formkit: 'textarea',
+    $formkit: 'text',
     name: 'company_director_name',
     label: 'ФИО',
     validation: 'required|length:5,100',
   },
   {
-    $formkit: 'textarea',
+    $formkit: 'text',
     name: 'company_director_position',
     label: 'Должность',
     validation: 'required|length:5,100',
   },
   {
-    $formkit: 'textarea',
+    $el: 'h1',
+    children: 'Контактное лицо',
+    attrs: {
+      class: 'text-md font-bold mb-2',
+    },
+  },
+  {
+    $formkit: 'text',
+    name: 'company_director_name',
+    label: 'ФИО',
+    validation: 'required|length:5,100',
+  },
+  {
+    $formkit: 'text',
     name: 'company_director_position',
-    label: 'Confirm password',
-    help: 'Enter your new password again to confirm it.',
-    validation: 'required|confirm',
-    validationLabel: 'password confirmation',
+    label: 'Должность',
+    validation: 'required|length:5,100',
+  },
+    {
+    $formkit: 'email',
+    name: 'company_email',
+    label: 'Email',
+    placeholder: 'test@mail.ru', 
+    validation: 'required|email',
   },
   {
-    $cmp: 'FormKit',
-    props: {
-      name: 'eu_citizen',
-      type: 'checkbox',
-      id: 'eu',
-      label: 'Are you a european citizen?',
-    },
-  },
-  {
-    $formkit: 'select',
-    if: '$get(eu).value',
-    name: 'cookie_notice',
-    label: 'Cookie notice frequency',
-    options: {
-      refresh: 'Every page load',
-      hourly: 'Ever hour',
-      daily: 'Every day',
-    },
-    help: 'How often should we display a cookie notice?',
+    $formkit: 'tel',
+    name: 'company_phone',
+    label: 'Телефон',
+    placeholder: '+79123456789',
+    validation: '/^\+?[0-9]{9-15}$/',
+    validationVisibility: 'dirty',
+    validationMessages: {
+      matches: 'Телефонный номер состоит из 9-15 цифр',
+    }
   },
 ]
